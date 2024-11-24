7fa7a8d05013bf46cd88005b6c47e0c5da26583d
ajmvalle <ajmvalle@gmail.com>
2024-11-24 16:01:05 -0600

    Nuevo proyecto Laravel Vue

---
+++ resources/js/Pages/Dashboard/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Head title="Dashboard" />
+    <h1 class="mb-8 text-3xl font-bold">Inicio</h1>
+    <p class="mb-8 leading-normal">Hola este es el inicio del proyecto MiCo</p>
+  </div>
+</template>
+
+<script>
+import { Head } from '@inertiajs/vue3'
+import Layout from '@/Shared/Layout.vue'
+
+export default {
+  components: {
+    Head,
+  },
+  layout: Layout,
+}
+</script>
